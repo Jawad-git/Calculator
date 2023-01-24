e9c5490ba11329b7b0db29260a1e1e9ac6183375
Jawad <94877975+Jawad-git@users.noreply.github.com>
2023-01-24 02:06:38 +0200

    make the calculator somewhat working

--- script.js
+++ script.js
@@ -41,22 +41,29 @@ function operate(a, operator, b)
     switch(operator)
     {
         case "+":
-            sum(a, b);
+            return sum(a, b);
             break;
         case "-":
-            subtract(a, b);
+            return subtract(a, b);
             break;
-        case "*":
-            multiply(a, b);
+        case "x":
+            return multiply(a, b);
             break;
-        case "/":
-            divide(a, b);
+        case "÷":
+            return divide(a, b);
             break;
         case "%":
-            modulo(a, b);
+            return modulo(a, b);
             break;
     }
 }
+
+//making array that stores the values entered by user
+let values = [30];
+let i = 0;
+let j = 0;
+let result = null;
+let comeon = 'whatever';
 
 //Making function that clears calculator display when we press "AC".
     const displaytop = document.querySelector('.head');
@@ -68,6 +75,12 @@ function operate(a, operator, b)
         displaytop.innerHTML = '';
         displaybottom.innerHTML = '';
         // Not finished: reset the values
+        i = 0; 
+        result = 0;
+        for (let k = 0; k < 30; k++)
+        {
+            values[k] = null; 
+        }
     });
 
 
@@ -78,11 +91,43 @@ function append(whatever)
     let child = document.createElement("span");
     child.textContent = whatever;
     displaytop.appendChild(child);
+    // populaate the array based on input, increment when an operator is inputted
+    if (whatever == 'x' || whatever == '÷' || whatever == '%' || whatever == '-' || whatever == '+')
+    {
+        values[++i] = whatever;
+        i++;
+    }
+    else
+    {
+        values[i] = Number(whatever) + (values[i] * 10);
+    }
 }
 //rest is written as inline in html 
     
 
+// trying out the calculation
+let o = 1;
+function computePair()
+{
+    return operate(values[o-1], values[o], values[o+1]);
+}
+
+function calculate()
+{
+    while (values[o] != null && values[o+1] != null)
+    {
+        let newresult = computePair();
+        values.splice(0, 3, `${newresult}`);
+    }
+    return values[o-1];
+}
 
 
-  //  let some = document.querySelector("body");
-   // some.textContent = 'whats going on';
+
+const equal = document.querySelector(".equal");
+equal.addEventListener("click", () => {
+result = calculate();
+resulttext = document.createElement('span');
+resulttext.textContent = `${result}`;
+displaybottom.appendChild(resulttext); 
+})
